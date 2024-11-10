2845e626aee6659e86022441781d943e74b8633e
DreamFlyingFlower <58282232@qq.com>
2024-11-11 00:31:05 +0800

    study something

--- dream-test-auth2/dream-test-auth2-core/src/test/java/com/wy/test/core/ApplicationTests.java
+++ dream-test-auth2/dream-test-auth2-core/src/test/java/com/wy/test/core/ApplicationTests.java
@@ -3,11 +3,16 @@ package com.wy.test.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import dream.flying.flower.binary.Base64Helper;
+
+//@SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	public static void main(String[] args) {
+		System.out.println(Base64Helper.encodeString("messaging-client:123456".getBytes()));
+	}
 }
